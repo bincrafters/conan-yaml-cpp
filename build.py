77fada9b487dc8105843763e0f70560a821ae895
Uilian Ries <uilian@khomp.com>
2017-10-24 13:22:13 -0200

    Use default build package tool
    Signed-off-by: Uilian Ries <uilian@khomp.com>

--- build.py
+++ build.py
@@ -1,26 +1,8 @@
 """This script build Conan.io package to multiple platforms."""
-from platform import system
-from os import getenv
 from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.password = getenv("CONAN_PASSWORD")
     builder.add_common_builds(shared_option_name="yaml-cpp:shared", pure_c=False)
-    if system() == "Linux":
-        stdlibcpp11_builds = []
-        for settings, options in builder.builds:
-            settings["compiler.libcxx"] = "libstdc++"
-            if settings["compiler.version"] > "4.9":
-                _settings = dict(settings)
-                _settings["compiler.libcxx"] = "libstdc++11"
-                stdlibcpp11_builds.append([_settings, options])
-        builder.builds = builder.builds + stdlibcpp11_builds
-    elif system() == "Windows":
-        windows_builds = []
-        for settings, options in builder.builds:
-            if not options["yaml-cpp:shared"] or not settings["compiler"] == "Visual Studio":
-                windows_builds.append([settings, options])
-        builder.builds = windows_builds
     builder.run()
